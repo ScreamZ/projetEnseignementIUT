2eb25479b73e22e251595dee8414663f74eba1df
ScreamZ <andreas.hanss67@gmail.com>
2015-02-03 15:20:43 +0100

    Fix inheritance on Enseignement using Single Table inheritance strategy

--- src/KMGH/AppBundle/Entity/Projet.php
+++ src/KMGH/AppBundle/Entity/Projet.php
@@ -3,34 +3,13 @@
 namespace KMGH\AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\Mapping\InheritanceType;
 
 /**
  * Projet
  *
  * @ORM\Table(name="Projet")
  * @ORM\Entity(repositoryClass="KMGH\AppBundle\Entity\ProjetRepository")
- * @InheritanceType("JOINED")
  */
 class Projet extends Enseignement
 {
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
 }
