bc8a4455c122d7fdae8fc79393bd8352609c9baa
ScreamZ <andreas.hanss67@gmail.com>
2015-02-08 14:32:01 +0100

    Default Value with none given

--- src/KMGH/AppBundle/Controller/AppController.php
+++ src/KMGH/AppBundle/Controller/AppController.php
@@ -2,8 +2,6 @@
 
 namespace KMGH\AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AppController extends Controller
